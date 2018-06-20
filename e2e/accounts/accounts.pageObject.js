671da15fef88a74c7ffdc7d5cd7893d0ccf3000a
Cyril Raffatelli <cyril.raffatelli@icloud.com>
2018-06-20 09:44:18 +0200

    Modified accounts page object

--- e2e/accounts/accounts.pageObject.js
+++ e2e/accounts/accounts.pageObject.js
@@ -10,8 +10,11 @@ var DashboardAccounts = function() {
 
   this.url = 'https://dashboard-dev.parkopoly.fr/#/accounts';
   this.searchBar = element(by.model('$select.search'));
-  this.searchBarDropdown = this.searchBar.$('[ng-show="$select.open"]');
+  this.searchBarDropdown = element(by.repeater('item in $select.items'));
+  this.searchBarDropdownAll = element.all(by.repeater('item in $select.items'));
   this.selectAllButton = $('[ng-click="sm.selectAll()"]');
+  this.toast = $('md-toast');
+  this.table = element(by.repeater('account in accountsCtrl.accountsToShow track by $index'));
   this.createAccountButton = $('[ng-click="accountsCtrl.createAccount()"]');
   this.createAccountDialog = $('[aria-describedby="dialogContent_create-account"]');
   this.createAccountDialogCloseButton = this.createAccountDialog.$('[ng-click="caCtrl.cancel()"]');
