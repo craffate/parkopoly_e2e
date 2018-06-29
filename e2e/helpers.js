395715753e9e112caf60323a82ab3a25ed9a5acc
Cyril Raffatelli <cyril.raffatelli@icloud.com>
2018-06-29 11:59:14 +0200

    Modified helper function

--- e2e/helpers.js
+++ e2e/helpers.js
@@ -2,4 +2,30 @@ module.exports = {
   getRandomInt: function(max) {
     return Math.floor(Math.random() * Math.floor(max));
   },
+  switchCheckbox: function(checkbox) {
+    checkbox.getAttribute('aria-checked').then(async function(res) {
+      await checkbox.click();
+    });
+  },
+  checkCheckbox: function(checkbox) {
+    checkbox.getAttribute('aria-checked').then(async function(res) {
+      if (res === 'false')
+        await checkbox.click();
+    });
+  },
+  uncheckCheckbox: function(checkbox) {
+    checkbox.getAttribute('aria-checked').then(async function(res) {
+      if (res === 'true')
+        await checkbox.click();
+    });
+  },
+  getFromDropdown: async function(s, arr) {
+    let ret = null;
+    for (i = 0; i < arr.length || ret !== null; i++) {
+      await new Promise(async function(res) {
+        ret = await arr[i].getAttribute('value') === s ? arr[i] : null;
+      });
+    };
+    return ret;
+  }
 };
