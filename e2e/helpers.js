85bb846ed0516408370a0ace5725a5f5053b8d15
Cyril Raffatelli <cyril.raffatelli@icloud.com>
2018-07-19 14:08:22 +0200

    Modified helpers

--- e2e/helpers.js
+++ e2e/helpers.js
@@ -1,14 +1,24 @@
+const path = require('path');
+
 module.exports = {
   getRandomInt: function(max) {
     return Math.floor(Math.random() * Math.floor(max));
   },
   checkCheckbox: async function(checkbox) {
-    if (await checkbox.getAttribute('aria-checked') === 'false')
-      await checkbox.click();
+    if (await checkbox.getAttribute('aria-checked') === 'false') {
+      return checkbox.click();
+    }
   },
   uncheckCheckbox: async function(checkbox) {
-    if (await checkbox.getAttribute('aria-checked') === 'true')
-      await checkbox.click();
+    if (await checkbox.getAttribute('aria-checked') === 'true') {
+      return checkbox.click();
+    }
+  },
+  switchCheckbox: async function(checkbox, b) {
+    const state = await (/true/).test(await checkbox.getAttribute('aria-checked'));
+    if (state !== b) {
+      return checkbox.click();
+    }
   },
   getFromDropdown: async function(s, arr) {
     return arr.filter(function(el, idx) {
@@ -16,5 +26,16 @@ module.exports = {
         return s === val;
       });
     });
+  },
+  displayUpload: async function(fileInput) {
+    return browser.executeScript('arguments[0].css("visibility", "visible"); arguments[0].css("display", "block");', fileInput);
+  },
+  uploadFile: async function(path, fileInput) {
+    const absolutePath = await path.resolve(__dirname, path);
+    return fileInput.sendKeys(absolutePath);
+  },
+  scrollIntoView: async function(el) {
+    const loc = await el.getLocation();
+    return browser.executeScript('window.scrollTo(' + loc.x + ', ' + loc.y + ');');
   }
 };
