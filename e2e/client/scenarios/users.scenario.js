8f6af85c9b7c4f4eeda29249bbe72589ca01cea7
Cyril Raffatelli <cyril.raffatelli@icloud.com>
2018-08-22 16:42:44 +0200

    Added client users scenario

---
+++ e2e/client/scenarios/users.scenario.js
@@ -0,0 +1,45 @@
+const helpers = require('../../helpers');
+const UsersPageObject = require('../page_objects/users.pageObject');
+const specData = require('../data/users.scenario.data.json');
+
+describe('Users', function() {
+  const usersPage = new UsersPageObject();
+
+  beforeAll(async function() {
+    await usersPage.getClick();
+  });
+
+  dataSpec(specData, (data, iteration) => {
+    describe(`Create ${data.firstName} ${data.lastName} user`, function() {
+      it('should open the user creating dialog', async function() {
+        await usersPage.newuserButton.click();
+      });
+
+      it('should fill the form', async function() {
+        await usersPage.lastnameInput.sendKeys(data.lastName + TIMESTAMP);
+        await usersPage.firstnameInput.sendKeys(data.firstName);
+        await usersPage.emailInput.sendKeys(data.email + TIMESTAMP);
+        await usersPage.companyInput.sendKeys(data.company);
+        await usersPage.functionInput.sendKeys(data.function);
+        await usersPage.brandSelect.click();
+        await usersPage.brandSelectSearch.sendKeys(data.brands[0]);
+        await usersPage.brandSelectSelectallButton.click();
+        await usersPage.concessionSelect.click();
+        await usersPage.concessionSelectSearch.sendKeys(data.concessions[0]);
+        await usersPage.concessionSelectSelectallButton.click();
+
+        if (data.notifications === true) {
+          await usersPage.notificationsCheckbox.click();
+        };
+
+        if (data.emailNotifications === true) {
+          await usersPage.emailCheckbox.click();
+        };
+      });
+
+      it('should submit the form', async function() {
+        await usersPage.createButton.click();
+      });
+    });
+  });
+});
