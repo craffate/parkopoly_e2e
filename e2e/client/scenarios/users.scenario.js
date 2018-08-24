5032421e8c62c29a3b11a27345bcd17dbffd0dbc
Cyril Raffatelli <cyril.raffatelli@icloud.com>
2018-08-24 17:30:28 +0200

    Fixed client users scenario

--- e2e/client/scenarios/users.scenario.js
+++ e2e/client/scenarios/users.scenario.js
@@ -9,24 +9,48 @@ describe('Users', function() {
     describe(`Create ${data.firstName} ${data.lastName} user`, function() {
       it('should navigate to the users page', async function() {
         await usersPage.getClick();
+        await helpers.waitForNo(usersPage.spinner);
       });
 
       it('should open the user creation dialog', async function() {
         await usersPage.newuserButton.click();
+        await helpers.waitForVisibility(usersPage.dialog);
+        await helpers.waitForNo(usersPage.spinner);
       });
 
       it('should fill the form', async function() {
+        await usersPage.selectFunction(data.function);
         await usersPage.lastnameInput.sendKeys(data.lastName + TIMESTAMP);
         await usersPage.firstnameInput.sendKeys(data.firstName);
         await usersPage.emailInput.sendKeys(data.email + TIMESTAMP);
         await usersPage.companyInput.sendKeys(data.company);
         await usersPage.functionInput.sendKeys(data.function);
-        await usersPage.brandSelect.click();
-        await usersPage.brandSelectSearch.sendKeys(data.brands[0]);
-        await usersPage.brandSelectSelectallButton.click();
-        await usersPage.concessionSelect.click();
-        await usersPage.concessionSelectSearch.sendKeys(data.concessions[0]);
-        await usersPage.concessionSelectSelectallButton.click();
+
+        await helpers.asyncForEach(data.brands, async (s) => {
+          const EC = protractor.ExpectedConditions;
+
+          await usersPage.brandSelect.click();
+          await usersPage.brandSelectSearch.sendKeys(s);
+          await browser.wait(EC.textToBePresentInElement(
+            usersPage.brandFirstResult.$('span'), s),
+            500, `Search for brand ${s} failed`);
+          return usersPage.brandSelectSelectallButton.click();
+        });
+
+        await helpers.asyncForEach(data.concessions, async (s) => {
+          const EC = protractor.ExpectedConditions;
+
+          await usersPage.concessionSelect.click();
+          await usersPage.concessionSelectSearch.sendKeys(s);
+          await browser.wait(EC.textToBePresentInElement(
+            usersPage.concessionFirstResult.$('span'), s),
+            500, `Search for concession ${s} failed`);
+          return usersPage.concessionSelectSelectallButton.click();
+        });
+
+        if (data.quickbooking === true) {
+          await usersPage.quickbookingCheckbox.click();
+        };
 
         if (data.notifications === true) {
           await usersPage.notificationsCheckbox.click();
@@ -39,6 +63,8 @@ describe('Users', function() {
 
       it('should submit the form', async function() {
         await usersPage.createButton.click();
+        await helpers.waitForNo(usersPage.spinner);
+        await helpers.waitForNoVisibility(usersPage.dialog);
       });
     });
   });
