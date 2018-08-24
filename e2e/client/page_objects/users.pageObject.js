742b864f73b1ffec58f725a3e34cb5de26386113
Cyril Raffatelli <cyril.raffatelli@icloud.com>
2018-08-24 17:29:46 +0200

    Updated client users page object

--- e2e/client/page_objects/users.pageObject.js
+++ e2e/client/page_objects/users.pageObject.js
@@ -3,18 +3,16 @@ const helpers = require('../../helpers');
 module.exports = function() {
   this.url = 'users';
   this.usersButton = $('a[href="/users"]');
+  this.spinner = $('div.spinner');
 
   this.searchform = $('par-text-search > form');
   this.searchformInput = this.searchform.$('input');
   this.searchformButton = this.searchform.$('button');
 
-  this.newuserButton = element(by.buttonText('Nouvel utilisateur'));
+  this.newuserButton = $('div.am-datatable-header').$('button.btn.btn-primary.btn-add');
 
   this.dialog = element(by.id('par-dialog'));
   this.dialogBody = this.dialog.$('.modal-body');
-  this.managerRadio = this.dialogBody.element(by.id('roleManager'));
-  this.userRadio = this.dialogBody.element(by.id('roleUser'));
-  this.adminRadio = this.dialogBody.element(by.id('roleAdmin'));
   this.quickbookingCheckbox = this.dialogBody.element(by.id('user-quick-booking'));
   this.accessCheckbox = this.dialogBody.element(by.id('user-access'));
   this.firstnameInput = this.dialogBody.element(by.id('firstName'));
@@ -26,10 +24,12 @@ module.exports = function() {
   this.brandSelect = this.dialogBody.element(by.id('brands-select'));
   this.brandSelectSearch = this.brandSelect.element(by.name('search'));
   this.brandSelectSelectallButton = this.brandSelect.$$('ul > li').first();
+  this.brandFirstResult = this.brandSelect.$$('ul > li').get(1);
 
   this.concessionSelect = this.dialogBody.element(by.id('concessions-select'));
   this.concessionSelectSearch = this.concessionSelect.element(by.name('search'));
   this.concessionSelectSelectallButton = this.concessionSelect.$$('ul > li').first();
+  this.concessionFirstResult = this.concessionSelect.$$('ul > li').get(1);
 
   this.notificationsCheckbox = this.dialogBody.element(by.id('notifications'));
   this.emailCheckbox = this.dialogBody.element(by.id('emailNotifyForPenalty'));
@@ -38,10 +38,16 @@ module.exports = function() {
   this.createButton = this.dialog.element(by.buttonText('Créer'));
 
   this.get = async function() {
-    await browser.get(this.url);
+    return browser.get(this.url);
   };
 
   this.getClick = async function() {
-    await this.usersButton.click();
+    return this.usersButton.click();
+  };
+
+  this.selectFunction = async function(f) {
+    return this.dialogBody.
+      element(by.cssContainingText('div.am-radio.inline > label', f)).
+      click();
   };
 }
