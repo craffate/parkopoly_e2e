6c9e3a55e93eef5605e36296f34f26aec48781e2
Cyril Raffatelli <cyril.raffatelli@icloud.com>
2018-09-04 16:33:19 +0200

    Fixed brand scenario

--- e2e/backoffice/scenarios/brand.scenario.js
+++ e2e/backoffice/scenarios/brand.scenario.js
@@ -10,36 +10,36 @@ describe('Brands', function() {
   beforeAll(async function() {
     await DashboardIngredients.get();
     await helpers.waitForSpinner();
+    await DashboardIngredients.searchbar.click();
+    await DashboardIngredients.searchbarDropdownBrand.click();
   });
 
   dataSpec(specData, (data, iteration) => {
     describe(`Create ${data.name} brand`, function() {
-      it('should select the brand option from the searchbar', async function() {
-        await DashboardIngredients.searchbar.click();
-        await DashboardIngredients.searchbarDropdownBrand.click();
-      });
-
       it('should fill the form', async function() {
+        let el;
+
         await brandPage.nameInput.sendKeys(data.name + TIMESTAMP);
         await brandPage.colorpickerInput.sendKeys(data.color);
         await brandPage.anticipationInput.sendKeys(data.min);
         await brandPage.logoSearchbar.click();
-        await brandPage.logoSearchbarInput.sendKeys(data.logo + TIMESTAMP);
-        await brandPage.logoSearchbarDropdown.click();
+        el = await helpers.deleteMe(data.logo + TIMESTAMP, brandPage.logoSearchbarDropdownAll);
+        await el[0].click();
+
         if (data.missionType !== null) {
-          helpers.asyncForEach(data.missionType, async (s) => {
-            await brandPage.missiontypeDropdown.click();
+          await helpers.asyncForEach(data.missionType, async (s) => {
             let el;
 
+            await brandPage.missiontypeDropdown.click();
             el = await helpers.getFromDropdown(s,
               brandPage.missiontypeDropdownTypesAll);
-            await s[0].click();
-            await s[0].sendKeys(protractor.Key.ESCAPE);
+            await el[0].click();
+            return el[0].sendKeys(protractor.Key.ESCAPE);
           });
-        }
+        };
+
         await brandPage.accountDropdown.click();
-        await helpers.waitForVisibility(
-          brandPage.accountDropdownInput, 1000, 'Menu didn\'t open in time');
+        await helpers.waitForVisibility(brandPage.accountDropdownInput);
         await brandPage.accountDropdownInput.sendKeys(TIMESTAMP);
         await brandPage.accountDropdownAccountsAll.first().click();
         await brandPage.penaltySearchbar.click();
