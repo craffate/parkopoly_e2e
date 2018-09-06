d78b8b0653cf109cca058400301bc10ff71493dc
Cyril Raffatelli <cyril.raffatelli@icloud.com>
2018-09-06 11:39:28 +0200

    Fixed drivers scenario

--- e2e/backoffice/scenarios/drivers.scenario.js
+++ e2e/backoffice/scenarios/drivers.scenario.js
@@ -13,20 +13,23 @@ describe('Drivers', function() {
     describe(`Create ${data.lname} ${data.fname} driver`, function() {
       it('should open the driver creation dialog', async function() {
         await driversPage.createDriverButton.click();
-        await helpers.waitFor(driversPage.createDriverDialog, 5000,
-          'Couldn\'t open the creation form dialog');
+        await helpers.waitFor(driversPage.createDriverDialog);
       });
 
       it('should fill the form', async function() {
+        let el;
+
         await driversPage.lastNameInput.sendKeys(data.lname);
         await driversPage.firstNameInput.sendKeys(data.fname);
         await driversPage.emailInput.sendKeys(data.email);
         await driversPage.addressInput.sendKeys(data.address);
+        await driversPage.addressInput.sendKeys(protractor.Key.DOWN);
+        await driversPage.addressInput.sendKeys(protractor.Key.ENTER);
         await driversPage.telInput.sendKeys(data.phone);
         await driversPage.status.click();
-        data.status = await helpers.getFromNonMaterialDropdown(
+        el = await helpers.getFromNonMaterialDropdown(
           data.status, 'status', driversPage.statusDropdownAll);
-        await data.status[0].click();
+        await el[0].click();
 
         if (data.badges !== null) {
           await helpers.asyncForEach(data.badges, async (s) => {
@@ -34,16 +37,16 @@ describe('Drivers', function() {
 
             el = await helpers.getFromDropdown(s, driversPage.badgeDropdownAll);
             await driversPage.badge.click();
-            await helpers.scrollIntoView(el);
-            await el.click();
+            await helpers.scrollIntoView(el[0]);
+            return el[0].click();
           });
         };
 
         await driversPage.pointsInput.sendKeys(data.points);
         await driversPage.occupation.click();
-        data.occupation = await helpers.getFromNonMaterialDropdown(
+        el = await helpers.getFromNonMaterialDropdown(
           data.occupation, 'p', driversPage.occupationDropdownAll);
-        await data.occupation[0].click();
+        await el[0].click();
         await driversPage.equipmentInput.sendKeys(data.uniform);
         await driversPage.commentInput.sendKeys(data.comment);
         await helpers.switchCheckbox(driversPage.unpaidSwitch, data.unpaid);
@@ -55,8 +58,7 @@ describe('Drivers', function() {
 
       it('should submit the form', async function() {
         await driversPage.submitButton.click();
-        await helpers.waitForNo(driversPage.createDriverDialog, 5000,
-          'Couldn\'t close the creation form dialog');
+        await helpers.waitForNo(driversPage.createDriverDialog);
       });
     });
   });
