5c6b6fe8b62d1fbbc00300e8897b02f57a7dede6
Cyril Raffatelli <cyril.raffatelli@icloud.com>
2018-10-05 09:37:55 +0200

    Updated delivery mission page objects

--- e2e/client/page_objects/missions/deliveryMission.pageObject.js
+++ e2e/client/page_objects/missions/deliveryMission.pageObject.js
@@ -3,7 +3,6 @@ const step2 = require('./deliveryMission.step2.pageObject');
 const step3 = require('./deliveryMission.step3.pageObject');
 const step4 = require('./deliveryMission.step4.pageObject');
 const step5 = require('./deliveryMission.step5.pageObject');
-const step6 = require('./deliveryMission.step6.pageObject');
 
 module.exports = function(Parent) {
   this.step1 = new step1(this);
@@ -11,7 +10,6 @@ module.exports = function(Parent) {
   this.step3 = new step3(this);
   this.step4 = new step4(this);
   this.step5 = new step5(this);
-  this.step6 = new step6(this);
 };
 
 module.exports = deliveryMission;
--- e2e/client/page_objects/missions/deliveryMission.step1.pageObject.js
+++ e2e/client/page_objects/missions/deliveryMission.step1.pageObject.js
@@ -11,6 +11,7 @@ module.exports = function(Parent) {
   this.replacementConcession = $('par-select#concessions-to-the-same-select');
   this.replacementConcessionInput = this.replacementConcession.$('input');
   this.replacementConcessionResults = this.replacementConcession.$$('a');
+  this.submitButton = $('button[type="submit"]');
 };
 
 module.exports = step1;
