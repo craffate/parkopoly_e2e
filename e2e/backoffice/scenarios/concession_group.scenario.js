d34ec44fcaa70825c538ce7c2d2034d68babfc52
Cyril Raffatelli <cyril.raffatelli@icloud.com>
2018-10-10 23:40:12 +0200

    Updated concession group scenario

--- e2e/backoffice/scenarios/concession_group.scenario.js
+++ e2e/backoffice/scenarios/concession_group.scenario.js
@@ -5,14 +5,12 @@ const specData = require('../data/concession_group.scenario.data.json');
 describe('Concession groups', function() {
   const concessionPage = new ConcessionPageObject();
 
-  dataSpec(specData, (data, iteration) => {
+  dataSpec(specData, (data) => {
     it(`Create ${data.name} concession group`, async function() {
       await concessionPage.get();
       await helpers.waitForSpinner();
-      if (iteration === 1) {
-        await concessionPage.pointOfSaleGroupButton.click();
-        await helpers.waitForSpinner();
-      };
+      await concessionPage.pointOfSaleGroupButton.click();
+      await helpers.waitForSpinner();
       await concessionPage.createPointOfSaleGroupButton.click();
       await helpers.waitForVisibility(concessionPage.createPointOfSaleGroupDialog);
       await concessionPage.pointOfSaleGroupNameInput.sendKeys(`${data.name} ${TIMESTAMP}`);
